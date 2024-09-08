3085075c730ad54228a0744c3ae7090fa4c2df24
Kaleb Lape <kaleb.lape@gmail.com>
2024-09-08 08:42:17 -0500

    feat: Add support for MinitestTestClass as a challengeable type

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,7 @@
 class Challenge < ApplicationRecord
   belongs_to :journey
   belongs_to :challengeable, polymorphic: true
+  delegated_type :challengeable, types: %w[MinitestTestClass]
 
   def self.new_from_minitest_test_method(record, position:)
     new(
